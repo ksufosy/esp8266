40552e413d8e17e6604f313dfd943a1a455caf0a
Jared Foster <Jared Foster>
2017-03-15 19:59:37 -0500

    Added webserver componets.

--- connectToNetworkTest/connectToNetworkTest.ino
+++ connectToNetworkTest/connectToNetworkTest.ino
@@ -1,5 +1,9 @@
+//credit to Rui Santos for the example that got this started. 
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
 
 ESP8266WebServer server;
 
@@ -17,9 +21,16 @@ void setup() {
   webhtml += "<h1>ESP8266 Webserver</h1><p>Pin 1<a href=\"pin1On\"><button>ON</button></a><\p><a href=\"pin1Off\"><button>OFF<\button><\a><\p>
   webhtml += "<p>Pin 2<a href=\"pin2On\"><button>ON<\button><\a><\p><a href=\"pin2Off\"><button>OFF</button><\a><\p>
 
-  
-  WiFi.begin(ssid,password);
+  //gpio setup
+  pinMode(gpio0_pin, OUTPUT);
+  digitalWrite(gpio0_pin, LOW);
+  pinMode(gpio1_pin, OUTPUT);
+  digitalWrite(gpio1_pin, LOW);
+
+  //Setup serial
   Serial.begin(115200);
+  //Setup wifi and connect
+  WiFi.begin(ssid,password);
   while(WiFi.status()!= WL_CONNECTED)
   {
     Serial.print(".");
@@ -28,6 +39,33 @@ void setup() {
   Serial.println("");
   Serial.print("IP Address: ");
   Serial.print(WiFi.localIP());
+  //DNS responder
+  if(mdns.begin("esp8266", WiFi.localIP()){
+      Serial.println("MDNS Started");
+  }
+  server.on("/", [](){
+    server.send(200,"text/html", webhtml);
+  });
+  server.on("/pin1On, [](){
+    server.send(200,"text/html", webhtml);
+    digitalWrite(gpio0_pin, HIGH);
+    delay(500);
+  });
+  server.on("/pin1Off, [](){
+    server.send(200,"text/html", webhtml);
+    digitalWrite(gpio0_pin, LOW);
+    delay(500);
+    });
+  server.on("/pin2On, [](){
+    server.send(200,"text/html", webhtml);
+    digitalWrite(gpio1_pin, HIGH);
+    delay(500);
+    });
+  server.on("/pin2Off, [](){
+    server.send(200,"text/html", webhtml);
+    digitalWrite(gpio1_pin, LOW);
+    delay(500);
+    });
 }
 
 void loop() {
